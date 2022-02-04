9966406ca686fa0519296ef3672ae38e42a7ea14
canel1125 <canel1125@outlook.es>
2022-02-04 11:41:27 -0300

    muestra alerta de error por front

--- frontend/src/components/UploadForm.vue
+++ frontend/src/components/UploadForm.vue
@@ -66,6 +66,7 @@ export default {
         })
         .catch(err => {
           console.log(err);
+          this.sendSlug("Error encontrado")
         })
     },
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,6 +3,10 @@
     <div class="alert alert-success alert-dismissible" v-if="uploadedSlug">
       <strong>Información guardada! </strong> La información se almacenó correctamente.
     </div>
+    <div class="alert alert-danger alert-dismissible" v-if="errorFound">
+      <strong>Hubo un error! </strong> La información no se pudo almacenar correctamente.
+    </div>
+    
     <UploadForm v-on:emitedSlug=" getSlug($event)"/>
     <ContractsTable :contractSlug=uploadedSlug />
   </div>
@@ -22,11 +26,15 @@ export default {
   },
   data(){
     return{
-      uploadedSlug: false
+      uploadedSlug: false,
+      errorFound: false
     }
   },
   methods:{
     getSlug: function(value){
+      if (value == "Error encontrado"){
+        this.errorFound = true;
+      }else
       this.uploadedSlug = value;
     }
   }
